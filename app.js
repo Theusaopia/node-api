263d71049f052ad6afdca10395a19e172531c7c0
Matheus Dias <matheusdyas007@hotmail.com>
2022-08-18 15:46:41 -0300

    logando com morgan e tratando erros

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 const rotaProdutos = require("./routes/produtos");
 const rotaPedidos = require("./routes/pedidos");
+
+app.use(morgan("dev"));
 
 app.use("/produtos", rotaProdutos);
 app.use("/pedidos", rotaPedidos);
@@ -11,6 +14,21 @@ app.use("/teste", (req, res, next) => {
   res.status(200).send({
     mensagem: "Ok, deu certo.",
   });
+});
+
+app.use((req, res, next) => {
+  const erro = new Error("Não encontrado");
+  erro.status = 404;
+  next(erro);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500)
+  return res.send({
+    erro: {
+      mensagem: error.message
+    }
+  })
 });
 
 module.exports = app;
